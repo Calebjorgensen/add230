1af9b120e0fbb7cd1e11cd481a1b7b975d4e8820
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-06-20 08:27:44 -0700

    links javascript updated

--- scripts/links.js
+++ scripts/links.js
@@ -1,4 +1,12 @@
 const baseURL = "https://calebjorgensen.github.io/wdd230/";
 
+const linksURL = "https://calebjorgensen.github.io/wdd230/data/links.json";
 
 
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
