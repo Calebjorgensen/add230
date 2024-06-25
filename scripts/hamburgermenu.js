61cec0d805bfd7ccd8d5427e0995b73318437b45
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-06-25 15:29:44 -0700

    hamburger button is working

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -1,7 +1,7 @@
-const mainnav = document.querySelector(".naviagion");
-const menubutton = document.querySelector("#menu");
+const hambutton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
 hambutton.addEventListener("click", () => {
-    mainnav.classList.toggle("show");
-    hambutton.classList.toggle("show");
+    navigation.classList.toggle("open");
+    hambutton.classList.toggle("open");
 })
