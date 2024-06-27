cc904c46266f38c2a28700950b9049b59105ada2
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-06-27 11:40:46 -0700

    added the weather API to home page

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,7 +9,8 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);//Remember to remove this later.
+            //console.log(data);
+            displayResults(data);
         }else {
             throw Error(await response.text());
         }
@@ -17,3 +18,14 @@ async function apiFetch() {
         console.log(error);
     }
 }
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt",desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
