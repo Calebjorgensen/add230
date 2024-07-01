f076da23d81c85e8101c81af1306680c2c399407
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-07-01 12:22:28 -0700

    udpated the directory and the member json

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,9 +1,36 @@
-const url = "https://calebjorgensen.github.io/wdd230/chamber/members.json";
+const url = "https://calebjorgensen.github.io/wdd230/chamber/data/members.json";
 
 const cards = document.querySelector("#cards");
 
 async function getMembersData() {
-    const response = await fetch("https://calebjorgensen.github.io/wdd230/chamber/members.json");
+    const response = await fetch(url);
     const data = await response.json();
-    conosole.table("data.members");
+    //console.log(data);
+    displayMembers(data.members);
 }
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let coverImage = document.createElement("img");
+        let fullName = document.createElement("h2");
+        let number = document.createElement("p")
+        
+
+        fullName.textContent = `${member.name}`;
+
+        number.textContent = `${member.phonenumbers}`;
+
+
+        coverImage.setAttribute("src",member.image);
+        
+
+        card.appendChild(fullName);
+        card.appendChild(coverImage);
+        card.appendChild(number);
+
+        cards.appendChild(card);
+    })
+}
+
+getMembersData();
